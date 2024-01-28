d5dc3c04401d3933b1cebc5c188baecbfaf8f441
anazirjonov <anazirjonov10@gmail.com>
2024-01-28 13:54:14 +0500

    masalalar ishlash

---
+++ index.js
@@ -0,0 +1,159 @@
+// 1-masala
+
+// function hajmTopish(number) {
+//   return number ** 3;
+// }
+
+// console.log(hajmTopish(3));
+// 2-masala
+function belgiTekshirish(satr, belgi) {
+  return satr.includes(belgi);
+}
+console.log(belgiTekshirish(ksnd));
+// 3-masala
+
+// function num(n = [8, 10, 2, 9]) {
+//   return n[0];
+// }
+
+// console.log(num());
+
+// 4-masal
+
+// function sonlarYigindisi(son) {
+//   let yigindi = 0;
+
+//   while (son > 0) {
+//     yigindi += son % 10;
+//     son = Math.floor(son / 10);
+//   }
+
+//   return yigindi;
+// }
+// console.log(sonlarYigindisi(44444444));
+
+// 5-masal
+
+// function kabisaYili(yil) {
+//   if (yil % 4 == 0 && yil % 100 != 0) {
+//     return "Kasaba yili";
+//   } else {
+//     return "Kasaba yili emas";
+//   }
+// }
+
+// console.log(kabisaYili(2024));
+
+// 6-masal
+
+// function berilganqimat(arg, arg1) {
+//   return Math.floor(Math.random() * (arg1 - arg)) + arg;
+// }
+
+// console.log(berilganqimat(12, 23));
+
+// 7-misol
+// function togriTortburchakMaydoni(a, b) {
+//   let maydon = a * b;
+//   return maydon;
+// }
+// console.log(togriTortburchakMaydoni(2, 5));
+
+//  8-masala
+
+// function temperatura(Farengeytn) {
+//   let res = Math.trunc(Farengeytn - 32) / 1.8;
+//   return res;
+// }
+// console.log(temperatura(111));
+
+// 9-masala
+
+// function uchburchak(a, b, c) {
+//   let javob = "Teng yonlik va Masshtabli uchburchak emas ";
+//   if (a + b == b + c) {
+//     javob = "Teng yonli uchburchak";
+//   } else if (a + b > b + c && b + c > a + c && a + c < a + b) {
+//     javob = "Masshtabli uchburchak";
+//   } else {
+//     javob = "Teng yonlik va Masshtabli uchburchak emas ";
+//   }
+//   return javob;
+// }
+
+// console.log(uchburchak(4, 4, 4));
+
+// 10-masala
+
+// function minimal(a, b) {
+//   if (a > b) {
+//     return b;
+//   } else {
+//     return a;
+//   }
+// }
+// console.log(minimal(9, 6));
+
+// 11-masala
+
+// function doyra(radius) {
+//   return 3.14 * radius;
+// }
+// console.log(doyra(5));
+
+// 12-masala
+
+// 13-masala
+
+// function Doyra(radius, balandlik) {
+//   return radius ** 2 * balandlik;
+// }
+// console.log(Doyra(4, 6));
+
+// 14-masala
+// 15-masala
+// function tasodifiyHarf() {
+//   const tasodifiyIndeks = Math.floor(Math.random() * 26);
+//   const tasodifiyHarf = String.fromCharCode(65 + tasodifiyIndeks);
+//   return tasodifiyHarf;
+// }
+// console.log(tasodifiyHarf(10));
+
+// 16-masala
+
+// function uchburchak(a, b, c) {
+//   return a + b + c;
+// }
+// console.log(uchburchak(10, 5, 2));
+
+// 17-masala
+
+// function toqMukammalKvadratTekshirish(son) {
+//   const ildiz = Math.sqrt(son);
+
+//   return ildiz % 1 === 0 && son % 2 === 1;
+
+// }
+// console.log(toqMukammalKvadratTekshirish());
+
+// 18-masala
+
+// function tasodifison(arg, arg1) {
+//   let result = "Tasodifiy toq son emas";
+//   let sum;
+//   sum = Math.floor(Math.random() * (arg1 - arg)) + arg;
+//   if (sum % 2 == 1) {
+//     result = `${sum} Tasodifiy toq son`;
+//   }
+//   return result;
+// }
+
+// console.log(tasodifison(15, 73));
+
+// 19-masala
+// function cone(radius, height) {
+//   return (1 / 3) * radius ** 2 * height;
+// }
+// console.log(cone(5, 5));
+
+// 20-masala
